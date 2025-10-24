d7899755e296120ff176888eefc992739e127780
Allan Kirui <allan.kirui@outlook.com>
2025-10-24 17:13:45 +0300

    chore(tailwind): add new breakpoints, custom color palette and fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,39 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    },
+    extend: {
+      fontFamily: {
+        sans: [
+          "IBM Plex Sans",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "sans-serif"
+        ],
+        birthstone: ["Birthstone Bounce"]
+      },
+      colors: {
+        black: "#020101",
+        jet: "#333333",
+        bone: "#e7d9cc",
+        cream: "#f5eddb",
+        crayola: "#f6c96e",
+        "eerie-black": "#1a1a1a",
+        "cosmic-latte": "#fefaea",
+        "chestnut-brown": "#5f4235",
+        "pale-brown": "#7a6153",
+        "pastel-orange": "#ff7f2a",
+        "pastel-yellow": "#f39852",
+        "red-munsell": "#ec003f"
+      }
+    }
   },
   plugins: []
 }
