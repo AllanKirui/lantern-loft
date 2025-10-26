b851c8e8b212840f60a40664ac96ab15232fe926
Allan Kirui <allan.kirui@outlook.com>
2025-10-26 18:57:39 +0300

    feat(main): register BaseIcon component globally

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from "vue"
 import "./index.css"
 import App from "./App.vue"
+import BaseIcon from "./components/base/BaseIcon.vue"
 import router from "./router"
 
 const app = createApp(App)
+
+// Globally registered components
+app.component("BaseIcon", BaseIcon)
+
 app.use(router)
 app.mount("#app")
