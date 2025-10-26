b498c415a4429b3406eb1b21345d972a2e9c2352
Allan Kirui <allan.kirui@outlook.com>
2025-10-26 18:54:09 +0300

    feat(BaseIcon): add reusable component for SVG icons

---
+++ frontend/src/components/base/BaseIcon.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+  <svg class="w-5 h-5" aria-hidden="true">
+    <use :href="`/src/assets/icons/ui/${name}.svg`"></use>
+  </svg>
+</template>
