9fc4bd9e98ce49e31d93e1253e52ab83055673ae
Allan Kirui <allan.kirui@outlook.com>
2025-10-30 13:12:52 +0300

    feat(nav): add reusable button component that toggles nav dropdowns

---
+++ frontend/src/components/layout/nav/DropdownButton.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Props {
+  belongsTo: "account" | "recent" | "wishlist"
+  activeDropdown: string | null
+  icon: string
+  text: string
+}
+
+defineProps<Props>()
+
+const emit = defineEmits(["toggle"])
+</script>
+
+<template>
+  <button
+    @click="emit('toggle', belongsTo)"
+    class="btn-hover py-2 px-4 hover:after:bg-eerie-black/75 flex items-center gap-2 group-hover:text-bone overflow-hidden duration-200"
+    :class="{
+      'bg-eerie-black/75 text-bone': activeDropdown === 'account'
+    }"
+    :aria-expanded="activeDropdown === 'account'"
+    aria-controls="account-menu"
+  >
+    <BaseIcon :name="icon" class="w-5 h-5" :stroke-width="3.5" />
+    <span>{{ text }}</span>
+    <BaseIcon
+      name="chevron"
+      class="w-[11px] h-[6.8px] text-bone duration-200"
+      :class="{ 'rotate-180': activeDropdown === 'account' }"
+      :stroke-width="5"
+    />
+  </button>
+</template>
