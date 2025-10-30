39345df5dcec5e44f393da6f235e719e0590ec92
Allan Kirui <allan.kirui@outlook.com>
2025-10-30 14:07:42 +0300

    refactor(nav): move top navigation markup to TopNav component

--- frontend/src/components/layout/NavBar.vue
+++ frontend/src/components/layout/NavBar.vue
@@ -1,60 +1,13 @@
 <script setup lang="ts">
-import Logo from "../common/Logo.vue"
+import TopNav from "./nav/TopNav.vue"
 import BottomNav from "./nav/BottomNav.vue"
 </script>
 
 <template>
+  <!-- TODO add a transition when resizing, so that the disappearing elements look smooth -->
   <header class="w-full bg-black text-crayola relative z-[100]">
     <!-- Top navigation -->
-    <div
-      class="wrapper relative flex items-center justify-between px-fluid-xs h-20"
-    >
-      <!-- Logo -->
-      <div class="w-[126px]">
-        <a href="/">
-          <span
-            class="absolute left-4 top-1/2 -translate-y-1/2 font-medium fs-logo-fallback whitespace-nowrap"
-            >Lantern Loft</span
-          >
-          <Logo />
-        </a>
-      </div>
-
-      <!-- Center links -->
-      <nav class="hidden md:flex gap-6">
-        <a href="/" class="fs-nav-lg">Home</a>
-        <a href="/collections" class="fs-nav-lg">Shop</a>
-      </nav>
-
-      <!-- Right buttons -->
-      <div class="flex items-center gap-4">
-        <a href="/cart" class="flex items-center gap-[10px]">
-          <div class="relative">
-            <BaseIcon name="cart" class="w-7 h-7" />
-            <div
-              class="absolute min-w-5 min-h-5 -top-1/3 -right-2 flex items-center justify-center bg-black rounded-full"
-            >
-              <span class="fs-nav-cart-count text-cosmic-latte">3</span>
-            </div>
-          </div>
-          <span class="fs-nav-lg">Cart</span>
-        </a>
-
-        <!-- Vertical divider -->
-        <span class="w-[1px] h-5 bg-crayola" />
-
-        <button
-          class="group btn-hover flex items-center justify-center w-8 h-8 hover:after:bg-eerie-black rounded-full"
-        >
-          <BaseIcon
-            name="search"
-            class="w-6 h-6 group-hover:text-cosmic-latte duration-200"
-            :stroke-width="4"
-          />
-          <span class="sr-only">Search</span>
-        </button>
-      </div>
-    </div>
+    <TopNav />
 
     <!-- Horizontal divider -->
     <span class="block w-full h-[1px] bg-jet" />
