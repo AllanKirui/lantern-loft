eb2fd228def45a32bf2676ed057f6c864e0e2f68
Allan Kirui <allan.kirui@outlook.com>
2025-11-02 22:30:43 +0300

    feat(nav): add workshop link

--- frontend/src/components/layout/nav/BottomNav.vue
+++ frontend/src/components/layout/nav/BottomNav.vue
@@ -134,6 +134,22 @@ function leave(el: Element, done: () => void) {
       </transition>
     </div>
 
+    <!-- Workshop link -->
+    <div>
+      <!-- TODO add path to admin login -->
+      <a
+        href="#"
+        class="btn-hover relative flex items-center gap-2 py-2 px-4 hover:text-bone hover:after:bg-eerie-black/75 duration-200 overflow-hidden"
+      >
+        <BaseIcon
+          name="bulb-off"
+          class="w-[22px] h-[22px]"
+          :stroke-width="1.3"
+        />
+        <span>The Workshop</span>
+      </a>
+    </div>
+
     <!-- Overlay -->
     <Teleport to="body">
       <BaseOverlay @close="closeDropdown" />
