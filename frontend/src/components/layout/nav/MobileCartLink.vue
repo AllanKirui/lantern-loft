bafa20ac3a8b247286f9f939d69cf9afb0483082
Allan Kirui <allan.kirui@outlook.com>
2025-11-05 18:37:26 +0300

    feat(nav): add MobileCartLink component

---
+++ frontend/src/components/layout/nav/MobileCartLink.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import BaseMobileNavButton from "./BaseMobileNavButton.vue"
+</script>
+
+<template>
+  <BaseMobileNavButton type="link" href="/cart" text="Cart">
+    <div class="relative">
+      <BaseIcon name="cart" class="w-5 h-5 sm:w-6 sm:h-6" :stroke-width="3.5" />
+      <!-- Items Count -->
+      <div
+        class="absolute -top-1/4 -right-2 min-w-4 min-h-4 flex items-center justify-center bg-black rounded-full"
+      >
+        <span class="fs-nav-cart-count text-cosmic-latte leading-none">3</span>
+      </div>
+    </div>
+  </BaseMobileNavButton>
+</template>
