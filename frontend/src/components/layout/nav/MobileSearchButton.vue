972c6fb66571984c64ba951bec163ef5840f821e
Allan Kirui <allan.kirui@outlook.com>
2025-11-05 18:37:54 +0300

    feat(nav): add MobileSearchButton component

---
+++ frontend/src/components/layout/nav/MobileSearchButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BaseMobileNavButton from "./BaseMobileNavButton.vue"
+</script>
+
+<template>
+  <BaseMobileNavButton type="button" text="Search">
+    <div>
+      <BaseIcon name="search" class="w-5 h-5 sm:w-6 sm:h-6" :stroke-width="4" />
+    </div>
+  </BaseMobileNavButton>
+</template>
