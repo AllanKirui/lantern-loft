514b1332a54dcb7b4bc9ffdfdef11973ac5d40a0
Allan Kirui <allan.kirui@outlook.com>
2025-11-05 18:32:39 +0300

    feat(nav): add MobileNav component

---
+++ frontend/src/components/layout/nav/MobileNav.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import MobileAccountButton from "./MobileAccountButton.vue"
+import MobileCartLink from "./MobileCartLink.vue"
+import MobileMenuButton from "./MobileMenuButton.vue"
+import MobileSearchButton from "./MobileSearchButton.vue"
+</script>
+
+<template>
+  <!-- Left buttons - Mobile -->
+  <div class="md:hidden flex items-center fluid-gap-xs">
+    <MobileMenuButton />
+    <MobileAccountButton />
+  </div>
+
+  <!-- Right buttons - Mobile -->
+  <div class="md:hidden flex items-center fluid-gap-xs">
+    <MobileCartLink />
+    <MobileSearchButton />
+  </div>
+</template>
