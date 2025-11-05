0e9a5b979b0cdb35a25b41a71662adc548c6e649
Allan Kirui <allan.kirui@outlook.com>
2025-11-05 18:16:56 +0300

    feat(logo): add logo fallback text, add dynamic bindings for href attribute and image height

--- frontend/src/components/common/Logo.vue
+++ frontend/src/components/common/Logo.vue
@@ -1,9 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  href: string
+  height?: string // tailwind classes e.g h-20, h-32, h-[100px]
+}>()
+</script>
+
 <template>
-  <div class="relative z-40">
-    <img
-      src="@/assets/img/logo.svg"
-      alt="Lantern Loft Logo"
-      class="h-20 w-auto"
-    />
-  </div>
+  <a :href="href">
+    <!-- Logo fallback text -->
+    <span
+      class="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 font-medium fs-logo-fallback whitespace-nowrap text-cosmic-latte"
+      >Lantern Loft</span
+    >
+
+    <!-- Logo -->
+    <div class="relative z-40">
+      <img
+        src="@/assets/img/logo.svg"
+        alt="Lantern Loft Logo"
+        class="w-auto"
+        :class="height ? height : 'h-20'"
+      />
+    </div>
+  </a>
 </template>
