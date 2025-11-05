8571c3a39d914b0fbddccc7d9c4da05fb706c5a7
Allan Kirui <allan.kirui@outlook.com>
2025-11-05 18:37:00 +0300

    feat(nav): add MobileAccountButton component

---
+++ frontend/src/components/layout/nav/MobileAccountButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import BaseMobileNavButton from "./BaseMobileNavButton.vue"
+</script>
+
+<template>
+  <BaseMobileNavButton type="button" text="Account">
+    <div>
+      <BaseIcon
+        name="avatar"
+        class="w-5 h-5 sm:w-6 sm:h-6"
+        :stroke-width="3.5"
+      />
+    </div>
+  </BaseMobileNavButton>
+</template>
