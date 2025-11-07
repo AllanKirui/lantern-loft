1c4bd7d0c73e1f16f31d3567103052de16a075a1
Allan Kirui <allan.kirui@outlook.com>
2025-11-07 13:18:59 +0300

    feat(nav): integrate mobileNav store, add pointer for active mobile dropdown

--- frontend/src/components/layout/nav/BaseMobileNavButton.vue
+++ frontend/src/components/layout/nav/BaseMobileNavButton.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
+import { computed } from "vue"
+import { useMobileNavStore } from "@/stores/mobileNav"
+
 defineOptions({ inheritAttrs: false })
 
-defineProps<{
+const props = defineProps<{
   type: "link" | "button"
   text: string
   href?: string
 }>()
 
-const emit = defineEmits<{
-  (e: "click", event: MouseEvent): void
-}>()
+const mobileNavStore = useMobileNavStore()
+
+const selected = computed(() => props.text.toLowerCase())
 </script>
 
 <template>
@@ -18,12 +21,24 @@ const emit = defineEmits<{
     :href="type === 'link' ? href : undefined"
     :type="type === 'button' ? 'button' : undefined"
     class="relative -bottom-1 flex flex-col items-center gap-2 hover:text-bone duration-200"
+    :class="{
+      'text-bone': mobileNavStore.activeDropdown === selected
+    }"
     v-bind="$attrs"
-    @click="type === 'button' && $emit('click', $event)"
   >
     <slot />
     <span class="fs-nav-sm uppercase leading-none">
       {{ text }}
     </span>
+
+    <!-- Pointer -->
+    <span
+      class="absolute left-1/2 -translate-x-1/2 w-3 h-3 rotate-45 bg-cosmic-latte duration-300"
+      :class="[
+        mobileNavStore.activeDropdown === selected
+          ? 'opacity-1 -bottom-[22px]'
+          : 'opacity-0 -bottom-8'
+      ]"
+    ></span>
   </component>
 </template>
