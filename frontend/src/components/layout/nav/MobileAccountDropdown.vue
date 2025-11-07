e37e41bbc6c412ac4a47610479b626ebac355f15
Allan Kirui <allan.kirui@outlook.com>
2025-11-07 13:24:41 +0300

    feat(nav): add MobileAccountDropdown component

---
+++ frontend/src/components/layout/nav/MobileAccountDropdown.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import BaseMobileDropdown from "./BaseMobileDropdown.vue"
+</script>
+
+<template>
+  <BaseMobileDropdown>
+    <ul class="">
+      <li
+        v-for="(_, index) in Array.from({ length: 5 }, (_, i) => i)"
+        :key="index"
+        class="px-4 py-3"
+        :style="{ transitionDelay: `${index * 65}ms` }"
+      >
+        <a href="#">Lorem ipsum dolor sit amet consectetur</a>
+      </li>
+    </ul>
+  </BaseMobileDropdown>
+</template>
