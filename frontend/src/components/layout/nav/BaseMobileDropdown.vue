48472e033cd1f8b3b353083aed13d045abc04d64
Allan Kirui <allan.kirui@outlook.com>
2025-11-07 13:22:48 +0300

    feat(nav): add reusable component for mobile nav dropdowns

---
+++ frontend/src/components/layout/nav/BaseMobileDropdown.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useMobileNavStore } from "@/stores/mobileNav"
+
+const mobileNavStore = useMobileNavStore()
+
+function uppercaseFirst(str: string) {
+  const firstChar = str.charAt(0)
+  return str.replace(firstChar, firstChar.toUpperCase())
+}
+</script>
+
+<template>
+  <div
+    class="absolute top-[80px] left-0 w-full h-[calc(100vh-80px)] bg-cosmic-latte text-chestnut-brown overflow-y-auto z-20"
+  >
+    <!-- Header -->
+    <div class="flex justify-between">
+      <h2>{{ uppercaseFirst(mobileNavStore.activeDropdown as string) }}</h2>
+      <button
+        class="btn-hover flex items-center justify-center w-8 h-8 hover:after:bg-cream rounded-full"
+      >
+        <BaseIcon
+          name="close"
+          class="w-[18px] h-[18px] duration-200"
+          :stroke-width="2.5"
+        />
+        <span class="sr-only">Close</span>
+      </button>
+    </div>
+
+    <slot />
+  </div>
+</template>
