739b2864d8f34fad3b071213b168036ff8310e7b
Allan Kirui <allan.kirui@outlook.com>
2025-11-10 17:22:47 +0300

    feat(BaseCloseButton): add reusable close button component

---
+++ frontend/src/components/base/BaseCloseButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const emit = defineEmits(["click"])
+</script>
+
+<template>
+  <button class="btn-rounded" @click="$emit('click')">
+    <BaseIcon
+      name="close"
+      class="w-[18px] h-[18px] duration-200"
+      :stroke-width="2.5"
+    />
+    <span class="sr-only">Close</span>
+  </button>
+</template>
