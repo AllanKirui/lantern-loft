1385da0939edd2d272de8491f7a0871765aeea8b
Allan Kirui <allan.kirui@outlook.com>
2025-11-10 11:32:54 +0300

    feat(nav): set dropdown element's id dynamically

--- frontend/src/components/layout/nav/BaseMobileDropdown.vue
+++ frontend/src/components/layout/nav/BaseMobileDropdown.vue
@@ -7,10 +7,28 @@ function uppercaseFirst(str: string) {
   const firstChar = str.charAt(0)
   return str.replace(firstChar, firstChar.toUpperCase())
 }
+
+function setElementId(activeDropdown: string) {
+  let id = ""
+
+  switch (activeDropdown) {
+    case "menu":
+      id = "mobile-menu"
+      break
+    case "account":
+      id = "mobile-account-menu"
+      break
+  }
+
+  return id
+}
 </script>
 
 <template>
-  <div class="mobile-nav-dropdown">
+  <div
+    :id="setElementId(mobileNavStore.activeDropdown as string)"
+    class="mobile-nav-dropdown"
+  >
     <!-- Header -->
     <div class="flex justify-between">
       <h2 class="fs-mobile-nav-h2 font-bold">
