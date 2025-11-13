0fd32de23ab284d7d0fa4a6774c3210b7c697c96
Allan Kirui <allan.kirui@outlook.com>
2025-11-13 12:36:57 +0300

    feat(footer): integrate StorefrontFooter component

--- frontend/src/layouts/StorefrontLayout.vue
+++ frontend/src/layouts/StorefrontLayout.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import StorefrontNav from "@/components/layout/storefront/StorefrontNav.vue"
+import StorefrontFooter from "@/components/layout/storefront/StorefrontFooter.vue"
 </script>
 
 <template>
   <div class="storefront-layout">
     <StorefrontNav />
+    <StorefrontFooter />
   </div>
 </template>
