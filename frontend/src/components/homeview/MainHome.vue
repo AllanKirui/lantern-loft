fab37c1eb314a82274b5393438eb23a7544e46ae
Allan Kirui <allan.kirui@outlook.com>
2025-11-16 17:12:41 +0300

    feat(homepage): add MainHome component and integrate NewArrivalsCarousel

---
+++ frontend/src/components/homeview/MainHome.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NewArrivalsCarousel from "./NewArrivalsCarousel.vue"
+</script>
+
+<template>
+  <main class="wrapper sp-mt-footer">
+    <NewArrivalsCarousel />
+  </main>
+</template>
