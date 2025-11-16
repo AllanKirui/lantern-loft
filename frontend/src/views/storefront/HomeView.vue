13ec4f057b201574e415c1f02671dd6e5f129810
Allan Kirui <allan.kirui@outlook.com>
2025-11-16 17:11:54 +0300

    feat(homepage): integrate MainHome component

--- frontend/src/views/storefront/HomeView.vue
+++ frontend/src/views/storefront/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import HeroSection from "@/components/homeview/HeroSection.vue"
+import MainHome from "@/components/homeview/MainHome.vue"
 </script>
 
 <template>
   <HeroSection />
+  <MainHome />
 </template>
