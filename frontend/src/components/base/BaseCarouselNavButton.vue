8b84c9c790c62963f7ae5559a4f6ffd5f3d26166
Allan Kirui <allan.kirui@outlook.com>
2025-11-16 17:26:54 +0300

    feat(homepage): add BaseCarouselNavButton component for carousel navigation

---
+++ frontend/src/components/base/BaseCarouselNavButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+interface Props {
+  productType: "new" | "featured"
+  direction: "left" | "right"
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <button
+    class="btn-hover flex items-center justify-center w-8 h-8 rounded-md hover:after:bg-cream text-chestnut-brown disabled:pointer-events-none disabled:text-chestnut-brown/30"
+    :class="[
+      direction === 'left' ? `${productType}-prev` : `${productType}-next`
+    ]"
+    disabled
+  >
+    <BaseIcon
+      name="chevron"
+      class="relative w-[12px] h-[7.4px] duration-200"
+      :class="[direction === 'left' ? 'right-[1px] rotate-90' : '-rotate-90']"
+      :stroke-width="6"
+    />
+    <span class="sr-only">{{
+      direction === "left" ? "Previous" : "Next"
+    }}</span>
+  </button>
+</template>
