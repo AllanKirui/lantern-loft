e196b733fb8a567dc68a20e37aef5580a5ac80cf
Allan Kirui <allan.kirui@outlook.com>
2025-11-17 11:53:08 +0300

    feat(homepage): add NewProductCard component

---
+++ frontend/src/components/organisms/NewProductCard.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+interface NewProduct {
+  id: number
+  title: string
+  description: string
+}
+
+interface Props {
+  index: number
+  product: NewProduct
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <article role="article" :aria-labelledby="`new-arrival-item-${index + 1}`">
+    <div class="relative">
+      <span class="absolute top-0 left-0 w-full h-full z-10"></span>
+      <span
+        class="absolute top-0 left-0 px-[5px] py-2 rotated-label fs-new-arrival-label font-medium italic bg-black text-crayola uppercase leading-none"
+        >New Arrival
+      </span>
+      <img
+        src="@/assets/img/storefront/products/4-recopyright.png"
+        :alt="product.title"
+        class="w-full h-auto object-cover"
+      />
+    </div>
+    <div class="text-center mt-2">
+      <BaseIcon name="hanging-bulb" class="w-[10px] h-7" />
+      <p class="text-xs uppercase font-medium mt-2">Why we love the</p>
+      <h4
+        :id="`new-arrival-item-${index + 1}`"
+        class="mt-1 md:mt-2 fs-new-product-title font-semibold"
+      >
+        {{ product.title }}
+      </h4>
+      <p class="max-w-xs mx-auto mt-3">
+        {{ product.description }}
+      </p>
+      <a
+        href="#"
+        class="link-hover font-medium mt-3 md:mt-4"
+        data-replace="Explore"
+      >
+        <span>Explore</span>
+      </a>
+    </div>
+  </article>
+</template>
+
+<style scoped>
+.rotated-label {
+  writing-mode: sideways-lr;
+}
+</style>
