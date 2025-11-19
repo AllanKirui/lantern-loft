680a9f55df82b5f8803c2bf5674886ece32f7e6f
Allan Kirui <allan.kirui@outlook.com>
2025-11-19 13:21:49 +0300

    feat(homepage): add button ripple styles to carousel buttons

--- frontend/src/components/base/BaseCarouselNavButton.vue
+++ frontend/src/components/base/BaseCarouselNavButton.vue
@@ -1,28 +1,42 @@
 <script setup lang="ts">
+import { ref } from "vue"
+
 interface Props {
   productType: "new" | "featured"
   direction: "left" | "right"
 }
 
 defineProps<Props>()
+
+const showRipple = ref(false)
+
+function handleClick() {
+  showRipple.value = !showRipple.value
+
+  setTimeout(() => {
+    showRipple.value = false
+  }, 320) // Set it to the duration of the circle-scale-up animation in index.css
+}
 </script>
 
 <template>
   <button
-    class="btn-hover flex items-center justify-center w-8 h-8 rounded-md hover:after:bg-cream text-chestnut-brown disabled:pointer-events-none disabled:text-chestnut-brown/30"
+    class="btn-hover has-ripple-primary flex items-center justify-center w-8 h-8 rounded-md hover:after:bg-cream text-chestnut-brown disabled:pointer-events-none disabled:text-chestnut-brown/30"
     :class="[
       direction === 'left' ? `${productType}-prev` : `${productType}-next`
     ]"
     disabled
+    @click="handleClick"
   >
     <BaseIcon
       name="chevron"
-      class="relative w-[12px] h-[7.4px] duration-200"
+      class="relative w-[12px] h-[7.4px] z-10"
       :class="[direction === 'left' ? 'right-[1px] rotate-90' : '-rotate-90']"
       :stroke-width="6"
     />
     <span class="sr-only">{{
       direction === "left" ? "Previous" : "Next"
     }}</span>
+    <span v-if="showRipple" class="circle"></span>
   </button>
 </template>
