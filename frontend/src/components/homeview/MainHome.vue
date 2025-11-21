438d821c0cdc96bfb6a9180604edbee3286cca33
Allan Kirui <allan.kirui@outlook.com>
2025-11-21 13:07:22 +0300

    feat(homepage): integrate FeatureCarousel component

--- frontend/src/components/homeview/MainHome.vue
+++ frontend/src/components/homeview/MainHome.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import FeaturedCarousel from "./FeaturedCarousel.vue"
 import NewArrivalsCarousel from "./NewArrivalsCarousel.vue"
 </script>
 
 <template>
   <main class="wrapper">
     <NewArrivalsCarousel />
+    <FeaturedCarousel />
   </main>
 </template>
