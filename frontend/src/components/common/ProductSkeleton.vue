95b759754f071821992c1e44f67f1a0d6722b4fa
Allan Kirui <allan.kirui@outlook.com>
2025-11-24 13:14:09 +0300

    feat(homepage): add ProductSkeleton component

---
+++ frontend/src/components/common/ProductSkeleton.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import PistonLoader from "./PistonLoader.vue"
+</script>
+
+<template>
+  <div class="bg-bone rounded-md overflow-hidden shadow-sm">
+    <div
+      class="relative aspect-square flex items-center justify-center bg-cream rounded-md"
+    >
+      <PistonLoader />
+    </div>
+    <div class="p-3 space-y-3">
+      <div class="skeleton"></div>
+      <div class="skeleton w-1/2"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.skeleton {
+  position: relative;
+  height: 0.875rem;
+  background-color: rgb(122, 97, 83, 0.3);
+  border-radius: 0.25rem;
+  overflow: hidden;
+}
+.skeleton::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: -100%;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(
+    90deg,
+    transparent,
+    rgba(122, 97, 83, 0.3),
+    transparent
+  );
+  animation: shimmer 1.5s infinite;
+}
+@keyframes shimmer {
+  100% {
+    left: 100%;
+  }
+}
+</style>
