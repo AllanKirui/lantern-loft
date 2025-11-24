a032c82311194682da1a2f8b12069de512150ade
Allan Kirui <allan.kirui@outlook.com>
2025-11-24 13:09:23 +0300

    feat(homepage): add PistonLoader component

---
+++ frontend/src/components/common/PistonLoader.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ color?: string }>(), {
+  color: "bg-pale-brown/50" // Tailwind bg classes
+})
+</script>
+
+<template>
+  <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div class="loader flex justify-between w-5 h-5">
+      <div
+        v-for="n in 3"
+        :key="n"
+        :class="['w-1 h-full origin-center animate-loading rounded', color]"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.loader div:nth-child(2) {
+  animation-delay: 0.15s;
+}
+.loader div:nth-child(3) {
+  animation-delay: 0.3s;
+}
+</style>
