e546b194af080115bb94fa977c83fbb43e4488c9
Allan Kirui <allan.kirui@outlook.com>
2026-01-24 13:09:53 +0300

    refactor(products): update faker generator for a product's model code

--- backend/database/factories/ProductFactory.php
+++ backend/database/factories/ProductFactory.php
@@ -33,7 +33,7 @@ class ProductFactory extends Factory
             'LMP-' .
                 ($category->slug === 'table-lamps' ? 'TBL' : 'FLR') .
                 '-' .
-                $this->faker->numberBetween(1000, 9999)
+                $this->faker->unique()->numberBetween(100, 999)
         );
 
         // Create an array of 100 elements with varying counts for each value
