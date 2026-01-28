7247e1925eb49a6335f78a2e9f8b72b713e584d6
Allan Kirui <allan.kirui@outlook.com>
2026-01-28 13:25:55 +0300

    feat(api): add route name to featured products endpoint

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -29,5 +29,5 @@ Route::get('/db-test', function () {
 Route::middleware('auth:sanctum')->get('/user', fn(Request $request) => $request->user());
 
 Route::get('/products/new-arrivals', [ProductController::class, 'newArrivals']);
-Route::get('/products/featured', [ProductController::class, 'featured']);
+Route::get('/products/featured', [ProductController::class, 'featured'])->name('products.featured');
 Route::apiResource('products', ProductController::class);
