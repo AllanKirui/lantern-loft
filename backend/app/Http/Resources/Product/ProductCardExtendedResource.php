863a2179e21da628a5b3ec6739025bf6593e8f56
Allan Kirui <allan.kirui@outlook.com>
2026-01-29 15:15:51 +0300

    feat(products): add ProductCardExtended resource for frontend payload

---
+++ backend/app/Http/Resources/Product/ProductCardExtendedResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Request;
+
+class ProductCardExtendedResource extends ProductCardBaseResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // TODO Replace fake data with data from reviews and finishes
+        $hasNoReviews = fake()->boolean(40);
+        $hasNoFinish = fake()->boolean(60);
+
+        // Merge the return array from ProductCardBaseResource with these additional fields
+        return array_merge(parent::toArray($request), [
+            'price' => $this->price,
+            'discount_price' => $this->discount_price,
+            'tagline' => $this->tagline,
+            'is_new' => $this->is_new,
+            'rating' => $hasNoReviews ? 0 : round(fake()->randomFloat(1, 3.5, 5), 1),
+            'review_count' => $hasNoReviews ? 0 : random_int(0, 500),
+            'finishes_count' => $hasNoFinish ? 0 : random_int(0, 100),
+        ]);
+    }
+}
