3aea284305c49f6aebc717d253e815817babb3f5
Allan Kirui <allan.kirui@outlook.com>
2026-01-30 16:36:18 +0300

    refactor(NewProductCard): update component to use ProductCardBase type, remove unecessary props

--- frontend/src/components/organisms/NewProductCard.vue
+++ frontend/src/components/organisms/NewProductCard.vue
@@ -1,27 +1,22 @@
 <script setup lang="ts">
-interface NewProduct {
-  id: number
-  title: string
-  description: string
-}
+import type { ProductCardBase } from "@/types/products/product-card-base"
 
 interface Props {
-  index: number
-  product: NewProduct
+  product: ProductCardBase
 }
 
 defineProps<Props>()
 </script>
 
 <template>
-  <article role="article" :aria-labelledby="`new-arrival-item-${index + 1}`">
+  <article role="article" :aria-labelledby="`new-arrival-item-${product.id}`">
     <div class="relative">
       <span class="absolute top-0 left-0 w-full h-full z-10"></span>
       <span class="new-arrival-label">New Arrival</span>
-      <figure class="relative aspect-square bg-cream rounded overflow-hidden">
+      <figure class="relative aspect-square bg-cream overflow-hidden">
         <img
           src="@/assets/img/storefront/products/4-recopyright.png"
-          :alt="product.title"
+          :alt="product.image?.alt"
           class="w-full h-auto object-cover"
           loading="lazy"
         />
@@ -31,16 +26,14 @@ defineProps<Props>()
       <BaseIcon name="hanging-bulb" class="w-[10px] h-8" />
       <p class="text-xs uppercase font-medium mt-2">Why we love the</p>
       <h4
-        :id="`new-arrival-item-${index + 1}`"
-        class="mt-1 md:mt-2 fs-new-product-title font-semibold"
+        :id="`new-arrival-item-${product.id}`"
+        class="mt-1 md:mt-2 fs-new-product-title font-semibold capitalize"
       >
-        {{ product.title }}
+        {{ product.name }}
       </h4>
-      <p class="max-w-xs mx-auto mt-3">
-        {{ product.description }}
-      </p>
+      <p class="max-w-xs mx-auto mt-3">{{ product.shortDescription }}</p>
       <a
-        href="#"
+        :href="`/collections/${product.slug}`"
         class="link-hover font-medium mt-3 md:mt-4"
         data-replace="Explore"
       >
