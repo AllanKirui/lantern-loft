c333087bf82f783ee2037b832875d87f4f750766
Allan Kirui <allan.kirui@outlook.com>
2026-02-01 18:32:30 +0300

    feat(products): add method for fetching featured products

--- frontend/src/services/productService.ts
+++ frontend/src/services/productService.ts
@@ -1,10 +1,18 @@
 import apiClient from "./api"
 import type { LaravelApiResponse } from "@/types/api/laravel-api-response"
 import type { ProductCardBase } from "@/types/products/product-card-base"
+import type { ProductCardExtended } from "@/types/products/product-card-extended"
 
 export async function fetchNewArrivals() {
   const res = await apiClient.get<LaravelApiResponse<ProductCardBase[]>>(
     "/products/new-arrivals"
+  )
+  return res.data.data
+}
+
+export async function fetchFeatured() {
+  const res = await apiClient.get<LaravelApiResponse<ProductCardExtended[]>>(
+    "/products/featured"
   )
   return res.data.data
 }
