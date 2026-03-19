7d3c4b99e777f0e9121a8c9594198c5889f70c5b
Allan Kirui <allan.kirui@outlook.com>
2026-03-19 18:11:48 +0300

    feat(collections): integrate PriceBlock component

--- frontend/src/components/organisms/ProductsGrid.vue
+++ frontend/src/components/organisms/ProductsGrid.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import { computed } from "vue"
+import { useRoute } from "vue-router"
 import type { ProductCardExtended } from "@/types/products/product-card-extended"
 import GridListProductCard from "./GridListProductCard.vue"
+import PriceBlock from "../common/PriceBlock.vue"
 
 interface Props {
   products: ProductCardExtended[]
 }
 defineProps<Props>()
+
+const route = useRoute()
+const routeName = computed(() => route.name as string)
 </script>
 
 <template>
@@ -23,7 +29,13 @@ defineProps<Props>()
             :product="product"
             class="animate-fade-in-down"
             :style="{ animationDelay: `${index * 0.1}s` }"
-          />
+          >
+            <PriceBlock
+              :route-name="routeName"
+              :price="product.price"
+              :sale-price="product.discountPrice"
+            />
+          </GridListProductCard>
         </div>
       </template>
     </div>
