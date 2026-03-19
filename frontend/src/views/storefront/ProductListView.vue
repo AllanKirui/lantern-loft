786e915a9905d57bb0baac66def53b1b602481fc
Allan Kirui <allan.kirui@outlook.com>
2026-03-19 12:38:54 +0300

    feat(collections): integrate CollectionsMain component

--- frontend/src/views/storefront/ProductListView.vue
+++ frontend/src/views/storefront/ProductListView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import CollectionsHeader from "@/components/productlistview/CollectionsHeader.vue"
+import CollectionsMain from "@/components/productlistview/CollectionsMain.vue"
 </script>
 
 <template>
   <div class="wrapper px-3.5">
     <CollectionsHeader />
+    <CollectionsMain />
   </div>
 </template>
