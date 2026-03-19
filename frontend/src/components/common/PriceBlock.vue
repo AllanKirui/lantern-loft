17451f5fe288fccf749d153ec5d3585ad6a15069
Allan Kirui <allan.kirui@outlook.com>
2026-03-19 18:09:13 +0300

    feat(collections): add reusable PriceBlock component

---
+++ frontend/src/components/common/PriceBlock.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+interface Props {
+  routeName: string
+  price: number
+  salePrice: number | null
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    <!-- Previous price -->
+    <div v-if="salePrice" class="flex items-center gap-2 flex-wrap mt-1">
+      <div
+        class="flex items-baseline whitespace-nowrap mt-[1px] font-semibold text-pale-brown"
+      >
+        <span class="mr-0.5 text-sm leading-none">Was kes</span>
+        <span
+          :class="[
+            'leading-none',
+            routeName === 'collections.show' ? 'fs-pdp-original-price' : ''
+          ]"
+          >{{ price.toLocaleString() }}</span
+        >
+      </div>
+
+      <!-- Savings -->
+      <div
+        :class="[
+          'flex items-baseline whitespace-nowrap px-1 rounded font-semibold text-cosmic-latte bg-red-pigment',
+          routeName === 'collections.show' ? 'py-[3px]' : 'py-0.5'
+        ]"
+      >
+        <span class="mr-0.5 text-sm leading-none">Save kes</span>
+        <span
+          :class="[
+            'leading-none',
+            routeName === 'collections.show' ? 'fs-pdp-savings' : ''
+          ]"
+          >{{ (price - salePrice).toLocaleString() }}</span
+        >
+      </div>
+    </div>
+
+    <!-- Current Price -->
+    <div class="mt-1 font-semibold">
+      <span
+        :class="[
+          'mr-0.5 leading-none',
+          routeName === 'collections.show'
+            ? 'fs-pdp-currency'
+            : 'fs-product-card-currency'
+        ]"
+        >kes</span
+      >
+      <span
+        :class="[
+          'leading-none',
+          routeName === 'collections.show'
+            ? 'fs-pdp-current-price'
+            : 'text-[22px]'
+        ]"
+        >{{ (salePrice || price).toLocaleString() }}</span
+      >
+    </div>
+  </div>
+</template>
