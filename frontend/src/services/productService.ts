88b60ea0d96c74539f2c8d052ce358ac434b42b4
Allan Kirui <allan.kirui@outlook.com>
2026-03-19 11:59:48 +0300

    feat(products): add method to fetch paginated products

--- frontend/src/services/productService.ts
+++ frontend/src/services/productService.ts
@@ -1,9 +1,18 @@
 import apiClient from "./api"
 import type { LaravelApiResponse } from "@/types/api/laravel-api-response"
+import type { LaravelPaginatedResponse } from "@/types/api/laravel"
 import type { ProductCardBase } from "@/types/products/product-card-base"
 import type { ProductCardExtended } from "@/types/products/product-card-extended"
 
 export const productService = {
+  async fetchAll(page = 1) {
+    const res = await apiClient.get<
+      LaravelPaginatedResponse<ProductCardExtended[]>
+    >(`/products?page=${page}`)
+
+    return res.data
+  },
+
   async fetchNewArrivals() {
     const res = await apiClient.get<LaravelApiResponse<ProductCardBase[]>>(
       "/products/new-arrivals"
