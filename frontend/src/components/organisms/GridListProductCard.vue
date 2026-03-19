fc8193aa8e2c2e10df3c4e71237630aec7270ba4
Allan Kirui <allan.kirui@outlook.com>
2026-03-19 22:29:26 +0300

    refactor(GridListProductCard): update component markup

--- frontend/src/components/organisms/GridListProductCard.vue
+++ frontend/src/components/organisms/GridListProductCard.vue
@@ -22,55 +22,46 @@ defineProps<Props>()
       :title="`Explore the ${product.name}`"
       class="group block"
     >
-      <div>
-        <!-- Product Image -->
-        <div class="relative rounded-md overflow-hidden">
-          <span class="absolute top-0 left-0 w-full h-full z-10"></span>
-          <span v-if="product.isNew" class="new-arrival-label"
-            >New Arrival</span
-          >
-          <div class="absolute right-2 top-2 z-20">
-            <WishlistButton />
-          </div>
-
-          <figure
-            class="relative aspect-square bg-cream rounded overflow-hidden"
-          >
-            <img
-              src="@/assets/img/storefront/products/4-recopyright.png"
-              class="w-full h-auto object-cover"
-              :alt="product.name"
-              loading="lazy"
-            />
-          </figure>
+      <!-- Product Image -->
+      <div class="relative rounded-md overflow-hidden">
+        <span class="absolute top-0 left-0 w-full h-full z-10"></span>
+        <span v-if="product.isNew" class="new-arrival-label">New Arrival</span>
+        <div class="absolute right-2 top-2 z-20">
+          <WishlistButton />
         </div>
 
-        <!-- Product Details -->
-        <div class="py-3 px-[10px] md:px-3">
-          <h4
-            :id="`product-${product.id}`"
-            class="fs-product-card-name font-semibold leading-snug capitalize"
-          >
-            {{ product.name }}
-          </h4>
-
-          <p class="flex items-center gap-1.5 mt-2 leading-none">
-            <StarRatingDisplay
-              :product-id="product.id"
-              :rating="product.rating"
-            />
-            <span
-              v-if="product.reviewCount > 0"
-              class="text-sm font-medium leading-none"
-              >({{ product.reviewCount.toLocaleString() }})</span
-            >
-          </p>
-
-          <PriceBlock
-            :price="product.price"
-            :sale-price="product.discountPrice"
+        <figure class="relative aspect-square bg-cream rounded overflow-hidden">
+          <img
+            src="@/assets/img/storefront/products/4-recopyright.png"
+            class="w-full h-auto object-cover"
+            :alt="product.name"
+            loading="lazy"
           />
+        </figure>
+      </div>
+
+      <!-- Product Details -->
+      <div class="py-3 px-[10px] md:px-3">
+        <h4
+          :id="`product-${product.id}`"
+          class="fs-product-card-name font-semibold leading-snug capitalize"
+        >
+          {{ product.name }}
+        </h4>
+
+        <div class="flex items-center gap-1.5 mt-2 leading-none">
+          <StarRatingDisplay :rating="product.rating" />
+          <span
+            v-if="product.reviewCount > 0"
+            class="text-sm font-medium leading-none"
+            >({{ product.reviewCount.toLocaleString() }})</span
+          >
         </div>
+
+        <PriceBlock
+          :price="product.price"
+          :sale-price="product.discountPrice"
+        />
       </div>
     </a>
   </article>
