cba37e87d3cf1a62b743404144febbe62f441219
Allan Kirui <allan.kirui@outlook.com>
2026-03-19 13:27:00 +0300

    feat(collections): add ProductsGrid component with integrated GridListProductCard

---
+++ frontend/src/components/organisms/ProductsGrid.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { ProductCardExtended } from "@/types/products/product-card-extended"
+import GridListProductCard from "./GridListProductCard.vue"
+
+interface Props {
+  products: ProductCardExtended[]
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <section>
+    <!-- TODO add info card -->
+
+    <!-- Products grid -->
+    <div
+      class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-4"
+      role="list"
+    >
+      <template v-for="(product, index) in products">
+        <div class="card-hover lg:max-w-3xl lg:mx-auto rounded-md">
+          <GridListProductCard
+            :product="product"
+            class="animate-fade-in-down"
+            :style="{ animationDelay: `${index * 0.1}s` }"
+          />
+        </div>
+      </template>
+    </div>
+  </section>
+</template>
