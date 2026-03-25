5550794be7e980d83ef6de0a5345c4bf048bb8ab
Allan Kirui <allan.kirui@outlook.com>
2026-03-25 09:57:34 +0300

    feat(stores): add filters store

---
+++ frontend/src/stores/filters.ts
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia"
+import { ref } from "vue"
+import { useOverlayStore } from "./overlay"
+
+export const useFiltersStore = defineStore("filters", () => {
+  const overlayStore = useOverlayStore()
+  const sidebarVisible = ref(false)
+
+  function toggleSidebar() {
+    overlayStore[sidebarVisible.value ? "close" : "open"]()
+    sidebarVisible.value = !sidebarVisible.value
+  }
+
+  function close() {
+    sidebarVisible.value = false
+  }
+
+  return { sidebarVisible, toggleSidebar, close }
+})
