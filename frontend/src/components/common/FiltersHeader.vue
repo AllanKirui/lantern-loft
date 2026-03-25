2787df5d3e066665fba9b7cdd1dc1b78e4aab55f
Allan Kirui <allan.kirui@outlook.com>
2026-03-25 10:13:08 +0300

    feat(collections): add FiltersHeader component

---
+++ frontend/src/components/common/FiltersHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useFiltersStore } from "@/stores/filters"
+import BaseCloseButton from "../base/BaseCloseButton.vue"
+
+const filtersStore = useFiltersStore()
+</script>
+
+<template>
+  <div class="relative p-4 text-center">
+    <h2 class="font-semibold fs-filters-sidebar-heading">Filters</h2>
+    <BaseCloseButton
+      class="absolute right-2 top-1/2 -translate-y-1/2 hover:after:bg-cream"
+      @click="filtersStore.close"
+    />
+  </div>
+</template>
