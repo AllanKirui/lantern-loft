ab755dc7931905c44b8f4fe9ca2793904f22d38a
Allan Kirui <allan.kirui@outlook.com>
2026-03-25 14:46:08 +0300

    feat(collections): add FilterCategory component

---
+++ frontend/src/components/common/FilterCategory.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { categories } from "@/types/products/product-filters"
+
+interface Props {
+  categories: categories
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="space-y-2 mt-2">
+    <button
+      v-for="cat in categories"
+      :key="cat.slug"
+      class="flex items-baseline gap-2 capitalize"
+    >
+      <span>{{ cat.name }}</span>
+      <!-- TODO add product count -->
+      <span class="text-sm text-pale-brown">{{
+        Math.round(5 + Math.random() * 20)
+      }}</span>
+    </button>
+  </div>
+</template>
