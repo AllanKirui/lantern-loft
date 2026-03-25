3f8d61b9560babe3875e715d6f324e3a94410d50
Allan Kirui <allan.kirui@outlook.com>
2026-03-25 08:57:09 +0300

    feat(collections): add CollectionsToolbar component

---
+++ frontend/src/components/productlistview/CollectionsToolbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- Sort and Filter controls -->
+  <div class="flex items-center justify-between gap-3 mt-3 custom-width">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+@media (max-width: 830px) {
+  .custom-width {
+    width: 100%;
+  }
+}
+@media (min-width: 831px) {
+  .custom-width {
+    width: unset;
+  }
+}
+</style>
