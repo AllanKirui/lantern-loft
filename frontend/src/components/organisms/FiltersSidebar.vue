e486853f7df729f1373e80476f9cc8923c34719f
Allan Kirui <allan.kirui@outlook.com>
2026-03-25 09:59:18 +0300

    feat(collections): add FiltersSidebar component with integrated filters store

---
+++ frontend/src/components/organisms/FiltersSidebar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useFiltersStore } from "@/stores/filters"
+
+const filtersStore = useFiltersStore()
+</script>
+
+<template>
+  <aside
+    v-if="filtersStore.sidebarVisible"
+    class="fixed left-0 top-0 w-96 h-screen bg-cosmic-latte z-[101] overflow-y-auto"
+  >
+    FiltersSidebar
+  </aside>
+</template>
