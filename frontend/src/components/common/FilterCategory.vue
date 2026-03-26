9d5793c732a804727b275cc10684f52390891a50
Allan Kirui <allan.kirui@outlook.com>
2026-03-26 10:57:34 +0300

    feat(collections): add icon indicator for active category, emit click event to run updateQuery

--- frontend/src/components/common/FilterCategory.vue
+++ frontend/src/components/common/FilterCategory.vue
@@ -3,9 +3,13 @@ import type { categories } from "@/types/products/product-filters"
 
 interface Props {
   categories: categories
+  query: Record<string, any>
 }
 
 defineProps<Props>()
+defineEmits<{
+  (e: "click", value: string): void
+}>()
 </script>
 
 <template>
@@ -13,13 +17,22 @@ defineProps<Props>()
     <button
       v-for="cat in categories"
       :key="cat.slug"
-      class="flex items-baseline gap-2 capitalize"
+      class="flex items-center capitalize"
+      @click="$emit('click', cat.slug)"
     >
-      <span>{{ cat.name }}</span>
-      <!-- TODO add product count -->
-      <span class="text-sm text-pale-brown">{{
-        Math.round(5 + Math.random() * 20)
-      }}</span>
+      <span class="flex items-baseline gap-2">
+        <span>{{ cat.name }}</span>
+        <!-- TODO add product count -->
+        <span class="text-sm text-pale-brown">{{
+          Math.round(5 + Math.random() * 20)
+        }}</span>
+      </span>
+
+      <BaseIcon
+        v-if="query.category?.includes(cat.slug)"
+        name="round-check"
+        class="w-4 h-4 ml-2.5"
+      />
     </button>
   </div>
 </template>
