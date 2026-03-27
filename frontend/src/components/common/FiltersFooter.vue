17dba3c5d88873123c2a66779c5d38026e03bec9
Allan Kirui <allan.kirui@outlook.com>
2026-03-27 18:21:03 +0300

    feat(collections): add FiltersFooter component with apply and clear filters buttons

---
+++ frontend/src/components/common/FiltersFooter.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+interface Props {
+  appliedCount: number
+  hasFilters: boolean
+}
+
+defineProps<Props>()
+defineEmits(["apply", "clear"])
+</script>
+
+<template>
+  <div class="px-4 py-5">
+    <div>
+      <button
+        :disabled="appliedCount === 0"
+        class="btn-primary w-full disabled:pointer-events-none"
+        @click="$emit('apply')"
+      >
+        Apply
+        <template v-if="appliedCount > 0"> ({{ appliedCount }}) </template>
+      </button>
+
+      <span
+        v-if="!hasFilters"
+        class="block mt-2 text-center italic text-sm animate-fade-in-down"
+        >Select some filters to apply</span
+      >
+    </div>
+
+    <button
+      v-if="hasFilters"
+      class="btn-secondary mt-3 py-[9px] w-full animate-fade-in-down"
+      @click="$emit('clear')"
+    >
+      Clear All
+    </button>
+  </div>
+</template>
