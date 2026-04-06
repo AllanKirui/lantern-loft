edaee6213dbf1cd3b85a05e6716fedca58191bf4
Allan Kirui <allan.kirui@outlook.com>
2026-04-06 11:23:43 +0300

    style(CollectionsToolbar): update component markup styles

--- frontend/src/components/productlistview/CollectionsToolbar.vue
+++ frontend/src/components/productlistview/CollectionsToolbar.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- Sort and Filter controls -->
-  <div class="flex items-center justify-between gap-3 mt-3 custom-width">
+  <div
+    class="flex items-center justify-between gap-3 animate-fade-in-down custom-width"
+  >
     <slot />
   </div>
 </template>
@@ -9,6 +11,7 @@
 @media (max-width: 830px) {
   .custom-width {
     width: 100%;
+    margin-top: 14px;
   }
 }
 @media (min-width: 831px) {
