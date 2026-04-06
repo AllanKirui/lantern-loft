20142949f582080e768b596d3e435474cb6e97be
Allan Kirui <allan.kirui@outlook.com>
2026-04-06 11:17:41 +0300

    feat(collections): add NoItemsFound cmp shown when there are no filter results or empty responses

---
+++ frontend/src/components/common/NoItemsFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-[45vh]">
+    <div class="animate-fade-in-down" :style="{ animationDelay: '0.1s' }">
+      <BaseIcon
+        name="bulb-off"
+        class="w-24 h-24 text-pale-brown"
+        :stroke-width="1.3"
+      />
+    </div>
+
+    <div
+      class="mt-5 fs-no-items-found text-center animate-fade-in-down"
+      :style="{ animationDelay: '0.2s' }"
+    >
+      <slot name="message" />
+    </div>
+  </div>
+</template>
