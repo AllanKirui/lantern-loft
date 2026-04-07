62ed60a0f59cfceab028438445d58d936b99d586
Allan Kirui <allan.kirui@outlook.com>
2026-04-07 23:47:10 +0300

    style(FiltersSidebar): update component styles

--- frontend/src/components/organisms/FiltersSidebar.vue
+++ frontend/src/components/organisms/FiltersSidebar.vue
@@ -129,7 +129,7 @@ function clearDraftFilters() {
 <template>
   <aside
     v-if="filtersStore.sidebarVisible"
-    class="fixed left-0 top-0 w-96 h-screen bg-cosmic-latte z-[101] overflow-y-auto"
+    class="fixed left-0 top-0 w-5/6 md:w-96 h-screen bg-cosmic-latte z-[101] overflow-y-auto"
   >
     <FiltersHeader />
 
