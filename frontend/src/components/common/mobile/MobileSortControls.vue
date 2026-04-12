84d48882ca1073e6a10346c6494213d684c49036
Allan Kirui <allan.kirui@outlook.com>
2026-04-12 18:36:47 +0300

    feat(collections): add MobileSortControls component

---
+++ frontend/src/components/common/mobile/MobileSortControls.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { inject } from "vue"
+import type { CollectionContext } from "@/types/collection"
+import type { ProductCardExtended } from "@/types/products/product-card-extended"
+import { useMobileSheets } from "@/composables/useMobileSheets"
+
+// Inject the collection instance coming from layout (StorefrontLayout.vue)
+const { perPage } =
+  inject<CollectionContext<ProductCardExtended>>("collection")!
+
+const { openSort, openPerPage } = useMobileSheets()
+</script>
+
+<template>
+  <div class="flex gap-2 items-center">
+    <button @click="openSort" class="mobile-sort-button">
+      <span>Sort by</span>
+      <BaseIcon name="chevron-alt" class="w-5 h-5 rotate-180" />
+    </button>
+
+    <button @click="openPerPage" class="mobile-sort-button">
+      <span>{{ perPage }}</span>
+      <BaseIcon name="chevron-alt" class="w-5 h-5 rotate-180" />
+    </button>
+  </div>
+</template>
