f31c988d0e28ad69e7928f3c7dafc5775a3ec9b9
Allan Kirui <allan.kirui@outlook.com>
2026-04-14 12:51:13 +0300

    style(BaseSelect): update component styles

--- frontend/src/components/base/BaseSelect.vue
+++ frontend/src/components/base/BaseSelect.vue
@@ -17,20 +17,32 @@ function onChange(event: Event) {
 </script>
 
 <template>
-  <div>
-    <label :for="selectId" class="mr-1" :class="{ 'sr-only': hideLabel }"
+  <div class="flex items-center gap-1.5 text-[15px]">
+    <label :for="selectId" :class="{ 'sr-only': hideLabel }"
       >{{ label }}:</label
     >
 
-    <select
-      :id="selectId"
-      class="p-1 font-medium rounded bg-transparent cursor-pointer"
-      v-model="modelValue"
-      @change="onChange"
-    >
-      <option v-for="opt in sortOptions" :key="opt.value" :value="opt.value">
-        {{ opt.label }}
-      </option>
-    </select>
+    <div class="relative">
+      <select
+        :id="selectId"
+        class="px-2 py-1 pr-7 appearance-none font-medium rounded bg-transparent cursor-pointer"
+        v-model="modelValue"
+        @change="onChange"
+      >
+        <option v-for="opt in sortOptions" :key="opt.value" :value="opt.value">
+          {{ opt.label }}
+        </option>
+      </select>
+
+      <!-- Custom arrow -->
+      <div
+        class="pointer-events-none absolute inset-y-0 right-1 flex items-center"
+      >
+        <BaseIcon
+          name="chevron-alt"
+          class="w-5 h-5 rotate-180 text-chestnut-brown"
+        />
+      </div>
+    </div>
   </div>
 </template>
