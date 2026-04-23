dd41ca46a6745e5b5c9843e6ab2d4c072031e3a0
Allan Kirui <allan.kirui@outlook.com>
2026-04-23 15:35:13 +0300

    feat(product-detail): add ProductDetailHero component, integrate product overview components

---
+++ frontend/src/components/productdetailview/ProductDetailHero.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ProductOverviewTop from "./ProductOverviewTop.vue"
+import ProductOverview from "./ProductOverview.vue"
+import ProductOverviewBottom from "./ProductOverviewBottom.vue"
+</script>
+
+<template>
+  <div class="sp-mt-pdp-hero flex gap-4 justify-center">
+    <section class="flex flex-col md:flex-row gap-4 overflow-hidden">
+      <!-- Product Overview Mobile: Name and Ratings -->
+      <div class="md:hidden">
+        <ProductOverviewTop />
+      </div>
+
+      <!-- Product Overview Mobile: Price and Add to Cart -->
+      <div class="md:hidden">
+        <!-- TODO add breadcrumbs here -->
+        <ProductOverviewBottom />
+      </div>
+    </section>
+
+    <!-- Product Overview: Desktop -->
+    <ProductOverview>
+      <ProductOverviewTop />
+      <ProductOverviewBottom />
+    </ProductOverview>
+  </div>
+</template>
