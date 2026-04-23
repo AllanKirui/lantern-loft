fcba182f110f2d6c5d9de58cf36ad4f29246f4d2
Allan Kirui <allan.kirui@outlook.com>
2026-04-23 15:48:34 +0300

    feat(product-detail): add AddToCartButton component

---
+++ frontend/src/components/productdetailview/AddToCartButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ showPrice?: boolean }>(), {
+  showPrice: false
+})
+</script>
+
+<template>
+  <button
+    class="btn-primary flex justify-center gap-2.5 w-full mx-auto sm_plus:max-w-96"
+  >
+    Add to Cart
+
+    <div v-if="showPrice" class="flex items-center gap-2.5">
+      <!-- Vertical Divider -->
+      <span class="w-[1px] h-5 bg-crayola/75" />
+
+      <div>
+        <span class="mr-[2px] text-sm leading-none">kes</span>
+        <span class="leading-none">{{ (5345).toLocaleString() }}</span>
+      </div>
+    </div>
+  </button>
+</template>
