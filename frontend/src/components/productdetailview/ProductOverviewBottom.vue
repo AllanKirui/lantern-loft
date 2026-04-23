7d448b5b6d2219c0ef3fc915e41293ca2695dff9
Allan Kirui <allan.kirui@outlook.com>
2026-04-23 15:48:01 +0300

    feat(product-detail): add ProductOverviewBottom component, integrate AddToCartButton component

---
+++ frontend/src/components/productdetailview/ProductOverviewBottom.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import PriceBlock from "../common/PriceBlock.vue"
+import AddToCartButton from "./AddToCartButton.vue"
+</script>
+
+<template>
+  <div class="mt-4">
+    <!-- Pricing -->
+    <PriceBlock :price="12345" :sale-price="null" :make-larger="true" />
+
+    <!-- Availability -->
+    <div class="mt-2">
+      <p class="text-green-700 text-sm">Available</p>
+    </div>
+
+    <!-- Shipping Info -->
+    <div class="mt-2">
+      <p class="text-sm">
+        <a href="#" data-replace="Shipping" class="link-hover font-medium"
+          ><span>Shipping</span></a
+        >
+        calculated at checkout.
+      </p>
+      <p class="mt-2 text-sm">Ships in 3-5 business days.</p>
+    </div>
+
+    <!-- Add to Cart -->
+    <div class="mt-5">
+      <AddToCartButton class="hidden md:flex" />
+      <AddToCartButton :show-price="true" class="flex md:hidden" />
+
+      <small class="block italic text-center mt-2">Cart coming soon.</small>
+    </div>
+  </div>
+</template>
