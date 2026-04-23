822331b26432124044776ae598fecd1f670e34b9
Allan Kirui <allan.kirui@outlook.com>
2026-04-23 15:31:34 +0300

    feat(product-detail): add ProductDetailView component, integrate ProductDetailHero component

---
+++ frontend/src/views/storefront/ProductDetailView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ProductDetailHero from "@/components/productdetailview/ProductDetailHero.vue"
+</script>
+
+<template>
+  <main id="main-content" class="wrapper px-3.5">
+    <!-- TODO add breadcrumbs here -->
+    <ProductDetailHero />
+  </main>
+</template>
