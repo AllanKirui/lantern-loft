8c9d4752bda68cd162b0fa34dae8250a453c8222
Allan Kirui <allan.kirui@outlook.com>
2026-04-26 20:15:17 +0300

    feat(products): add ProductDetailResource for frontend payload

---
+++ backend/app/Http/Resources/Product/ProductDetailResource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use App\Http\Resources\Review\ReviewResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'tagline' => $this->tagline,
+            'price' => round(floatVal($this->price)),
+            'discount_price' => $this->discount_price ? round(floatVal($this->discount_price)) : null,
+            'description' => $this->description,
+            'is_new' => $this->is_new,
+            'model_code' => $this->model_code,
+            'sku' => $this->sku,
+            'details' => $this->details,
+            'specs' => $this->specs,
+            'reviews' => [
+                'items' => ReviewResource::collection($this->whenLoaded('reviews')),
+                'count' => $this->whenLoaded('reviews', function () {
+                    return $this->reviews_count;
+                }),
+                'average_rating' => $this->whenLoaded('reviews', function () {
+                    return $this->reviews_avg_rating
+                        ? round($this->reviews_avg_rating, 1)
+                        : null;
+                })
+            ]
+        ];
+    }
+}
