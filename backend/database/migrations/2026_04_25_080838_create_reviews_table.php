d4343f3549ef669bd33ca650eae51c109c5ea053
Allan Kirui <allan.kirui@outlook.com>
2026-04-26 20:07:13 +0300

    feat(database): create reviews table

---
+++ backend/database/migrations/2026_04_25_080838_create_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->unsignedTinyInteger('rating'); // 1–5
+            $table->string('review_title')->nullable();
+            $table->text('comment');
+            $table->string('author_name', 100);
+            $table->boolean('recommends')->default(false);
+            $table->boolean('is_verified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
