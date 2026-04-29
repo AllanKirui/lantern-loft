44c11386386a8075d65b07affeb5d858d3fcdcf5
Allan Kirui <allan.kirui@outlook.com>
2026-04-29 12:01:56 +0300

    refactor(ProductOverviewTop): use dynamic product data coming from injected composable instance

--- frontend/src/components/productdetailview/ProductOverviewTop.vue
+++ frontend/src/components/productdetailview/ProductOverviewTop.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
+import { computed, inject } from "vue"
+import type { ProductDetailContext } from "@/types/product-detail"
+import type { Product } from "@/types/products"
 import StarRatingDisplay from "@/components/common/StarRatingDisplay.vue"
 import WishlistButton from "../common/WishlistButton.vue"
 
-// TODO add dynamic product data
-const product = {
-  name: "Aurora Desk Lamp"
-}
+// inject the productDetail instance coming from ProductDetailView.vue
+const { product } = inject<ProductDetailContext<Product>>("productDetail")!
+
+const tagline = computed(() => {
+  const p = product.value
+  return p.tagline.charAt(0).toUpperCase() + p.tagline.slice(1)
+})
+
+const rating = computed(() => product.value.reviews.averageRating)
+
+const hasReviews = computed(() => product.value.reviews.count > 0)
+
+const reviewsLinkText = computed(() => {
+  const count = product.value.reviews.count
+  return count > 1 ? `${count.toLocaleString()} reviews` : `${count} review`
+})
 </script>
 
 <template>
   <div>
     <span
+      v-if="product.isNew"
       class="py-[3px] px-2 text-[11.4px] font-medium bg-black text-crayola uppercase rounded"
       >New</span
     >
@@ -22,18 +38,19 @@ const product = {
         </h1>
 
         <!-- Tagline -->
-        <!-- TODO use dynamic data -->
         <p class="mt-1 font-medium fs-pdp-tagline">
-          The perfect glow for every moment
+          {{ tagline }}
         </p>
 
         <!-- Product Model, SKU -->
         <div class="mt-1">
-          <!-- TODO use dynamic data -->
           <small class="mr-3"
-            ><strong class="font-medium">Model:</strong> GLH-A1</small
+            ><strong class="font-medium">Model:</strong>
+            {{ product.modelCode }}</small
           >
-          <small><strong class="font-medium">SKU:</strong> 76342</small>
+          <small
+            ><strong class="font-medium">SKU:</strong> {{ product.sku }}</small
+          >
         </div>
       </div>
 
@@ -46,19 +63,20 @@ const product = {
     <!-- Ratings -->
     <div class="flex items-center gap-4 flex-wrap mt-2 md:mt-3">
       <div class="flex items-center gap-2">
-        <!-- TODO use dynamic data -->
-        <span class="font-medium fs-pdp-rating leading-none">3.7</span>
-        <StarRatingDisplay :rating="3.7" :size="22" class="md:hidden" />
-        <StarRatingDisplay :rating="3.7" :size="24" class="hidden md:flex" />
+        <span class="font-medium fs-pdp-rating leading-none">
+          {{ rating }}</span
+        >
+        <StarRatingDisplay :rating="rating" :size="22" class="md:hidden" />
+        <StarRatingDisplay :rating="rating" :size="24" class="hidden md:flex" />
       </div>
 
       <a
-        v-if="true"
+        v-if="hasReviews"
         href="#"
         class="flex-shrink-0 font-medium fs-pdp-reviews-count link-hover-alt"
-        data-replace="1,234 reviews"
+        :data-replace="reviewsLinkText"
       >
-        <span>{{ (1234).toLocaleString() }} reviews</span>
+        <span>{{ reviewsLinkText }}</span>
       </a>
       <a
         v-else
