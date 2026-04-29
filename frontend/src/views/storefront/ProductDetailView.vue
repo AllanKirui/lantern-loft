cf7a48e3901af57d089f6d74b980188631bdc522
Allan Kirui <allan.kirui@outlook.com>
2026-04-29 12:01:24 +0300

    feat(product-detail): update markup with loading status, integrate NoItemsFound component

--- frontend/src/views/storefront/ProductDetailView.vue
+++ frontend/src/views/storefront/ProductDetailView.vue
@@ -5,6 +5,7 @@ import { useProductDetail } from "@/composables/useProductDetail"
 import { productService } from "@/services/productService"
 import type { Product } from "@/types/products"
 import ProductDetailHero from "@/components/productdetailview/ProductDetailHero.vue"
+import NoItemsFound from "@/components/common/NoItemsFound.vue"
 
 const route = useRoute()
 
@@ -26,8 +27,32 @@ watch(
 </script>
 
 <template>
-  <main id="main-content" class="wrapper px-3.5">
+  <!-- TODO add a PDP skeleton -->
+  <div v-if="productDetail.isLoading.value">PDP loading...</div>
+
+  <main
+    v-else-if="productDetail.product.value"
+    id="main-content"
+    class="wrapper px-3.5"
+  >
+    <RouterLink to="/collections/at-iure-voluptatibus-195"
+      >At iure voluptatibus</RouterLink
+    >
     <!-- TODO add breadcrumbs here -->
     <ProductDetailHero />
   </main>
+
+  <NoItemsFound v-else class="h-[65vh]">
+    <template #message>
+      <div>
+        <h1 class="text-2xl font-medium">Oops!</h1>
+        <p class="mt-3">Looks like the lights flickered.</p>
+        <RouterLink
+          to="/collections"
+          class="btn-primary mt-6 text-base mx-auto max-w-96"
+          >Continue Shopping</RouterLink
+        >
+      </div>
+    </template>
+  </NoItemsFound>
 </template>
