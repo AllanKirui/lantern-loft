75b9ec8b1a1829b2fc9f5657d42a1f0011de4417
Allan Kirui <allan.kirui@outlook.com>
2026-04-29 11:53:15 +0300

    feat(products): add index.ts to export product types

---
+++ frontend/src/types/products/index.ts
@@ -0,0 +1,4 @@
+export type { ProductCardBase } from "./product-card-base"
+export type { ProductCardExtended } from "./product-card-extended"
+export type { ProductFilters } from "./product-filters"
+export type { Product } from "./product"
