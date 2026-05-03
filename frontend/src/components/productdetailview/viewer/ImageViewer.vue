e99f24795756a53d654e09cf0a83d6efbcfa4cf9
Allan Kirui <allan.kirui@outlook.com>
2026-05-04 00:04:32 +0300

    feat(product-detail): integrate BaseModal and ImageViewerContent component

---
+++ frontend/src/components/productdetailview/viewer/ImageViewer.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import BaseModal from "@/components/base/BaseModal.vue"
+import ImageViewerContent from "./ImageViewerContent.vue"
+</script>
+
+<template>
+  <BaseModal name="imageViewer" :coverFullScreenOnMobile="true">
+    <ImageViewerContent />
+  </BaseModal>
+</template>
