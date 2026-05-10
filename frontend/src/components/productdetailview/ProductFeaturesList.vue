b148e112bb40decb59cd1bc44cd0040230a2694a
Allan Kirui <allan.kirui@outlook.com>
2026-05-10 12:28:26 +0300

    feat(product-detail): add ProductFeaturesList component

---
+++ frontend/src/components/productdetailview/ProductFeaturesList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { inject } from "vue"
+import type { ProductDetailContext } from "@/types/product-detail"
+import type { Product } from "@/types/products"
+
+// inject the productDetail instance coming from ProductDetailView.vue
+const { product } = inject<ProductDetailContext<Product>>("productDetail")!
+</script>
+
+<template>
+  <section class="sp-mt-storefront-section max-w-4xl mx-auto">
+    <h2 class="font-semibold text-lg">Product details</h2>
+
+    <ul class="mt-3 list-disc pl-6">
+      <li
+        v-for="(detail, index) in product.details"
+        :key="index"
+        class="leading-normal"
+      >
+        {{ detail }}
+      </li>
+    </ul>
+  </section>
+</template>
