8a4a7e41dd78feada7080a8a68ca59e59b6effeb
Allan Kirui <allan.kirui@outlook.com>
2026-05-10 12:18:08 +0300

    feat(product-detail): add ProductDescription component

---
+++ frontend/src/components/productdetailview/ProductDescription.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { inject } from "vue"
+import type { ProductDetailContext } from "@/types/product-detail"
+import type { Product } from "@/types/products"
+
+// inject the productDetail instance coming from ProductDetailView.vue
+const { product } = inject<ProductDetailContext<Product>>("productDetail")!
+</script>
+
+<template>
+  <section class="sp-mt-storefront-section max-w-4xl mx-auto">
+    <h2 class="font-semibold text-lg">About this item</h2>
+    <div>
+      <p class="mt-3">
+        {{ product.description }}
+      </p>
+    </div>
+  </section>
+</template>
