3335b679d0e2deab39e1306bf50e3aa06701cea5
Allan Kirui <allan.kirui@outlook.com>
2026-05-15 11:19:34 +0300

    feat(product-detail): add RecommendationPercentage component

---
+++ frontend/src/components/productdetailview/reviews/RecommendationPercentage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{ percentage: number }>()
+</script>
+
+<template>
+  <div v-if="percentage > 0" class="items-center gap-2 font-medium">
+    <BaseIcon name="round-check" class="w-5 h-5 md:w-6 md:h-6" />
+
+    <div class="flex items-center gap-3">
+      <span class="fs-pdp-reviews-recommendation-percentage"
+        >{{ percentage }}%</span
+      >
+      <span class="fs-pdp-reviews-recommendation-percentage-text"
+        >of reviewers would <br />recommend this to a friend</span
+      >
+    </div>
+  </div>
+</template>
