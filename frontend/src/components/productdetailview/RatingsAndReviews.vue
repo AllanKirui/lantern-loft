e8991bd2acecfa6951d7ab4902bdfde40172fba1
Allan Kirui <allan.kirui@outlook.com>
2026-05-17 10:36:14 +0300

    feat(product-detail): handle case where no reviews are available for a product

--- frontend/src/components/productdetailview/RatingsAndReviews.vue
+++ frontend/src/components/productdetailview/RatingsAndReviews.vue
@@ -6,11 +6,13 @@ import SectionHeader from "@/components/common/SectionHeader.vue"
 import RatingsDistribution from "./reviews/RatingsDistribution.vue"
 import ReviewList from "./reviews/ReviewList.vue"
 import ReviewItem from "./reviews/ReviewItem.vue"
+import NoReviewItemsFound from "./reviews/NoReviewItemsFound.vue"
 
 // inject the productDetail instance coming from ProductDetailView.vue
 const { product } = inject<ProductDetailContext<Product>>("productDetail")!
 
 const reviews = computed(() => product.value.reviews)
+const hasReviews = computed(() => product.value.reviews.meta.count > 0)
 
 const MAX_REVIEWS_SHOWN = 7
 const count = computed(() => reviews.value.meta.count)
@@ -33,13 +35,21 @@ const subtitle = computed(() => {
   return `Displaying 1-7 of ${count.value.toLocaleString()} reviews`
 })
 
-const sectionHeaderData = computed(() => ({
-  tagline: "Hear it from our customers",
-  headingId: "ratings-and-reviews",
-  title: "Customers are saying",
-  subtitle: subtitle.value,
-  link: showViewMoreLink.value ? reviewsLink.value : undefined
-}))
+const sectionHeaderData = computed(() => {
+  if (hasReviews.value)
+    return {
+      tagline: "Hear it from our customers",
+      headingId: "ratings-and-reviews",
+      title: "Customers are saying",
+      subtitle: subtitle.value,
+      link: showViewMoreLink.value ? reviewsLink.value : undefined
+    }
+  else
+    return {
+      tagline: "Hear it from our customers",
+      headingId: "ratings-and-reviews"
+    }
+})
 
 const viewedLabel = computed(() => {
   if (count.value <= MAX_REVIEWS_SHOWN) return "You've viewed all reviews"
@@ -62,37 +72,43 @@ const viewMoreLink = computed(() => ({
   >
     <SectionHeader :data="sectionHeaderData">
       <template #heading>Ratings & Reviews<br />by Lanterneers</template>
-      <template #extras>
+      <template v-if="hasReviews" #extras>
         <RatingsDistribution :reviews-meta="reviews.meta" />
       </template>
     </SectionHeader>
 
-    <ReviewList>
-      <ReviewItem
-        v-for="(review, index) in reviews.items"
-        :key="review.id"
-        :review="review"
-        :is-last-item="index + 1 === reviews.items.length"
-      />
-    </ReviewList>
+    <template v-if="hasReviews">
+      <ReviewList>
+        <ReviewItem
+          v-for="(review, index) in reviews.items"
+          :key="review.id"
+          :review="review"
+          :is-last-item="index + 1 === reviews.items.length"
+        />
+      </ReviewList>
 
-    <div
-      class="sp-mt-pdp-view-more-reviews-text flex items-center justify-between"
-    >
       <div
-        class="fs-pdp-view-more-reviews-text text-pale-brown font-medium leading-none"
+        class="sp-mt-pdp-view-more-reviews-text flex items-center justify-between"
       >
-        {{ viewMoreLink.label }}
-      </div>
-
-      <div v-if="showViewMoreLink">
-        <a
-          :href="viewMoreLink.to"
-          :data-replace="viewMoreLink.text"
-          class="link-hover fs-pdp-view-more-reviews-text font-medium text-pale-brown"
-          ><span>{{ viewMoreLink.text }}</span></a
+        <div
+          class="fs-pdp-view-more-reviews-text text-pale-brown font-medium leading-none"
         >
+          {{ viewMoreLink.label }}
+        </div>
+
+        <div v-if="showViewMoreLink">
+          <a
+            :href="viewMoreLink.to"
+            :data-replace="viewMoreLink.text"
+            class="link-hover fs-pdp-view-more-reviews-text font-medium text-pale-brown"
+            ><span>{{ viewMoreLink.text }}</span></a
+          >
+        </div>
       </div>
-    </div>
+    </template>
+
+    <template v-else>
+      <NoReviewItemsFound />
+    </template>
   </section>
 </template>
