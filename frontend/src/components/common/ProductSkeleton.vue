9af4312416c3be470e4b0578db5959280ec7f2e5
Allan Kirui <allan.kirui@outlook.com>
2026-05-19 13:30:15 +0300

    refactor(ProductSkeleton): refactor skeleton component for use on product detail page

--- frontend/src/components/common/ProductSkeleton.vue
+++ frontend/src/components/common/ProductSkeleton.vue
@@ -1,70 +1,175 @@
-<script setup lang="ts">
-import { computed } from "vue"
-import { useLayoutStore } from "@/stores/layout"
-import PistonLoader from "./PistonLoader.vue"
-
-interface Props {
-  isForNewProducts?: boolean
-  useAdaptiveLayout?: boolean
-}
-
-const props = withDefaults(defineProps<Props>(), {
-  useAdaptiveLayout: false,
-  isForNewProducts: false
-})
-
-const layoutStore = useLayoutStore()
-const layout = computed(() => {
-  if (props.useAdaptiveLayout) return layoutStore.layout
-  else return "grid"
-})
-</script>
-
 <template>
-  <div
-    :class="[
-      'bg-bone/65 rounded-md overflow-hidden shadow-sm',
-      layout === 'grid'
-        ? ''
-        : 'lg:max-w-3xl mx-auto grid grid-cols-5 sm_plus:grid-cols-4'
-    ]"
-  >
+  <div class="wrapper px-3.5">
+    <!-- Breadcrumbs for large screens -->
     <div
-      :class="[
-        'relative aspect-square flex items-center justify-center bg-cream rounded-md',
-        layout === 'grid' ? '' : 'col-span-2 sm_plus:col-span-1'
-      ]"
+      class="skeleton hidden md:flex md:w-4/12 lg:w-3/12 h-3 mt-3 bg-cream rounded"
+    ></div>
+
+    <!-- Hero Section -->
+    <div
+      class="flex flex-col md:flex-row justify-center max-w-[1080px] mx-auto sp-mt-pdp-hero"
     >
-      <PistonLoader />
+      <!-- Overview Top Mobile -->
+      <div class="md:hidden w-full space-y-5">
+        <div class="space-y-3.5">
+          <!-- Title -->
+          <div class="skeleton w-1/2 sm_plus:w-5/12 h-5 bg-cream rounded"></div>
+
+          <!-- Tagline -->
+          <div
+            class="skeleton w-8/12 sm_plus:w-1/2 h-3.5 bg-cream rounded"
+          ></div>
+
+          <!-- Model & SKU -->
+          <div class="flex gap-3">
+            <div
+              class="skeleton w-1/4 sm_plus:w-1/6 h-3 bg-cream rounded"
+            ></div>
+            <div
+              class="skeleton w-1/4 sm_plus:w-1/6 h-3 bg-cream rounded"
+            ></div>
+          </div>
+
+          <!-- Rating -->
+          <div class="flex gap-3">
+            <div
+              class="skeleton w-2/6 sm_plus:w-3/12 h-4 bg-cream rounded"
+            ></div>
+            <div
+              class="skeleton w-2/6 sm_plus:w-3/12 h-4 bg-cream rounded"
+            ></div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Thumbnails for large screens -->
+      <div class="hidden lg:flex flex-col space-y-3">
+        <div class="skeleton aspect-square h-20 bg-cream rounded"></div>
+        <div class="skeleton aspect-square h-20 bg-cream rounded"></div>
+        <div class="skeleton aspect-square h-20 bg-cream rounded"></div>
+      </div>
+
+      <div class="flex md:flex-col justify-center w-full">
+        <!-- Thumbnails for mid screens 640px and 768px -->
+        <div
+          class="hidden sm_plus:flex flex-col md:hidden mt-5 md:mt-0 space-y-3"
+        >
+          <div class="skeleton aspect-square h-20 bg-cream rounded"></div>
+          <div class="skeleton aspect-square h-20 bg-cream rounded"></div>
+          <div class="skeleton aspect-square h-20 bg-cream rounded"></div>
+        </div>
+
+        <!-- Image -->
+        <div class="mt-5 md:mt-0 w-full">
+          <div
+            class="skeleton mx-auto w-full sm_plus:max-w-lg sm_plus:w-10/12 aspect-square bg-cream rounded-lg"
+          ></div>
+        </div>
+
+        <!-- Thumbnails container for medium screens -->
+        <div class="ml-[10%] hidden md:flex gap-3 mt-4 lg:hidden">
+          <div class="skeleton aspect-square h-20 bg-cream rounded"></div>
+          <div class="skeleton aspect-square h-20 bg-cream rounded"></div>
+          <div class="skeleton aspect-square h-20 bg-cream rounded"></div>
+        </div>
+      </div>
+
+      <!-- Overview Desktop -->
+      <div
+        class="hidden md:block md:max-w-[280px] lg:max-w-xs w-full h-fit space-y-5 p-4 bg-cream rounded-lg"
+      >
+        <div class="space-y-3.5">
+          <!-- Title -->
+          <div class="skeleton w-3/4 h-5 bg-bone/65 rounded"></div>
+
+          <!-- Tagline -->
+          <div class="skeleton w-full h-3.5 bg-bone/65 rounded"></div>
+
+          <!-- Model & SKU -->
+          <div class="flex gap-3">
+            <div class="skeleton w-1/4 h-3 bg-bone/65 rounded"></div>
+            <div class="skeleton w-1/4 h-3 bg-bone/65 rounded"></div>
+          </div>
+        </div>
+
+        <!-- Rating -->
+        <div class="flex gap-3">
+          <div class="skeleton w-2/6 h-4 bg-bone/65 rounded"></div>
+          <div class="skeleton w-2/6 h-4 bg-bone/65 rounded"></div>
+        </div>
+
+        <!-- Price -->
+        <div class="skeleton w-1/2 h-5 bg-bone/65 rounded"></div>
+
+        <!-- Availability & Shipping -->
+        <div class="space-y-3">
+          <div class="skeleton w-1/4 h-3 bg-bone/65 rounded"></div>
+          <div class="skeleton w-5/6 h-3 bg-bone/65 rounded"></div>
+          <div class="skeleton w-2/3 h-3 bg-bone/65 rounded"></div>
+        </div>
+
+        <!-- Button -->
+        <div class="skeleton w-full h-12 bg-bone/65 rounded"></div>
+      </div>
+
+      <!-- Breadcrumbs for small screens -->
+      <div
+        class="skeleton md:hidden w-8/12 sm_plus:w-1/2 h-3 mt-3 bg-cream rounded"
+      ></div>
+
+      <!-- Overview Bottom Mobile -->
+      <div class="md:hidden mt-5 w-full space-y-5">
+        <!-- Price -->
+        <div class="skeleton w-1/2 sm_plus:w-1/5 h-5 bg-cream rounded"></div>
+
+        <!-- Availability & Shipping -->
+        <div class="space-y-3">
+          <div class="skeleton w-1/4 sm_plus:w-1/5 h-3 bg-cream rounded"></div>
+          <div class="skeleton w-5/6 sm_plus:w-1/2 h-3 bg-cream rounded"></div>
+          <div class="skeleton w-2/3 sm_plus:w-2/6 h-3 bg-cream rounded"></div>
+        </div>
+
+        <!-- Button -->
+        <div
+          class="skeleton w-full sm_plus:max-w-96 mx-auto h-12 bg-cream rounded"
+        ></div>
+      </div>
     </div>
 
-    <div
-      :class="[
-        'p-3 space-y-3',
-        layout === 'grid' ? '' : 'col-span-3 w-full my-auto'
-      ]"
-    >
-      <template v-if="layout === 'grid'">
-        <template v-if="isForNewProducts">
-          <div class="skeleton w-1/2 mx-auto"></div>
-          <div class="skeleton w-5/6 mx-auto"></div>
-          <div class="skeleton w-5/6 mx-auto"></div>
-          <div class="skeleton w-5/6 mx-auto"></div>
-        </template>
-        <template v-else>
-          <div class="skeleton"></div>
-          <div class="skeleton w-1/2"></div>
-        </template>
-      </template>
+    <!-- Description -->
+    <div class="sp-mt-storefront-section max-w-4xl mx-auto">
+      <div class="space-y-4">
+        <div class="skeleton w-5/12 md:w-3/12 h-5 bg-cream"></div>
 
-      <template v-else>
-        <div class="skeleton w-1/2"></div>
-        <div class="skeleton w-1/3"></div>
-        <div class="skeleton w-full"></div>
-        <div class="skeleton w-full"></div>
-        <div class="skeleton w-3/4"></div>
-        <div class="skeleton w-1/4"></div>
-      </template>
+        <div class="space-y-3">
+          <div class="skeleton w-full h-3.5 bg-cream"></div>
+          <div class="skeleton w-full h-3.5 bg-cream"></div>
+          <div class="skeleton md:hidden w-full h-3.5 bg-cream"></div>
+          <div class="skeleton w-1/2 h-3.5 bg-cream"></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Details -->
+    <div class="sp-mt-storefront-section max-w-4xl mx-auto">
+      <div class="space-y-4">
+        <div class="skeleton w-5/12 md:w-3/12 h-5 bg-cream"></div>
+
+        <div class="space-y-3">
+          <div class="skeleton w-7/12 md:w-4/12 h-3.5 bg-cream"></div>
+          <div class="skeleton w-7/12 md:w-4/12 h-3.5 bg-cream"></div>
+          <div class="skeleton w-7/12 md:w-4/12 h-3.5 bg-cream"></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Specs -->
+    <div class="sp-mt-storefront-section max-w-4xl mx-auto">
+      <div class="space-y-4">
+        <div class="skeleton w-5/12 md:w-3/12 h-5 bg-cream"></div>
+
+        <div class="skeleton w-full h-64 bg-cream"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -72,8 +177,6 @@ const layout = computed(() => {
 <style scoped>
 .skeleton {
   position: relative;
-  height: 0.875rem;
-  background-color: rgb(122, 97, 83, 0.3);
   border-radius: 0.25rem;
   overflow: hidden;
 }
@@ -87,7 +190,7 @@ const layout = computed(() => {
   background: linear-gradient(
     90deg,
     transparent,
-    rgba(122, 97, 83, 0.3),
+    rgba(122, 97, 83, 0.15),
     transparent
   );
   animation: shimmer 1.5s infinite;
