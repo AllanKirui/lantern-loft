5778e0c9b7a295dabf1bb94944ff270a2b6ef2bf
Allan Kirui <allan.kirui@outlook.com>
2026-05-24 17:32:43 +0300

    feat(auth): add AuthLayout component, integrate AuthNav, RouterView and AuthFooter components

---
+++ frontend/src/layouts/AuthLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AuthNav from "@/components/layout/auth/AuthNav.vue"
+import AuthFooter from "@/components/layout/auth/AuthFooter.vue"
+</script>
+
+<template>
+  <div class="relative min-h-screen flex flex-col bg-black text-cosmic-latte">
+    <AuthNav />
+
+    <RouterView />
+
+    <AuthFooter />
+  </div>
+</template>
