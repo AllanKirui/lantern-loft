4d669791e0b0981f2cedf3b75dcda024e3d44961
Allan Kirui <allan.kirui@outlook.com>
2026-05-24 17:30:23 +0300

    feat(auth): add AuthFormSection component

---
+++ frontend/src/components/authview/AuthFormSection.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ title: string }>()
+</script>
+
+<template>
+  <div class="flex-1">
+    <h2 class="hidden md:block text-lg font-medium">{{ title }}</h2>
+
+    <div class="space-y-7 mt-5 md:mt-8">
+      <slot />
+    </div>
+  </div>
+</template>
