1885f818efb6d56e00125d2f4a9f98241fb21089
Allan Kirui <allan.kirui@outlook.com>
2026-05-24 17:28:29 +0300

    feat(auth): add AuthFooter component

---
+++ frontend/src/components/layout/auth/AuthFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="pb-10 text-bone">
+    <p class="sp-mt-footer-copy fs-footer-copy text-center">
+      © 2026, Lantern Loft. All Rights Reserved.
+    </p>
+  </footer>
+</template>
