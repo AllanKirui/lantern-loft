7bdd1fe660492bf96ad3ae60b9177418d79b2208
Allan Kirui <allan.kirui@outlook.com>
2026-05-24 17:28:01 +0300

    feat(auth): add AuthNav component

---
+++ frontend/src/components/layout/auth/AuthNav.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Logo from "@/components/common/Logo.vue"
+</script>
+
+<template>
+  <header class="relative pt-5 px-4 w-full z-[100]">
+    <div class="w-[126px] mx-auto">
+      <Logo href="/" />
+    </div>
+  </header>
+</template>
