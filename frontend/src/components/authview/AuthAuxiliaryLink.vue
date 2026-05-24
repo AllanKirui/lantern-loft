1b0fceecad46603f57fa18e6f2a5aea636f60aa0
Allan Kirui <allan.kirui@outlook.com>
2026-05-24 17:31:02 +0300

    feat(auth): add AuthAuxiliaryLink component

---
+++ frontend/src/components/authview/AuthAuxiliaryLink.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ to: string; title: string }>()
+</script>
+
+<template>
+  <p class="text-center text-sm text-bone">
+    <slot />
+
+    <RouterLink :to="to" :data-replace="title" class="link-hover text-crayola">
+      <span>{{ title }}</span>
+    </RouterLink>
+  </p>
+</template>
