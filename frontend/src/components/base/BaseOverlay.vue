d811a0f2b13f988044ce13f6c998c6774a822a49
Allan Kirui <allan.kirui@outlook.com>
2026-05-25 13:16:33 +0300

    style(BaseOverlay): update overlay styles

--- frontend/src/components/base/BaseOverlay.vue
+++ frontend/src/components/base/BaseOverlay.vue
@@ -9,7 +9,10 @@ const overlayStore = useOverlayStore()
     <transition name="fade">
       <div
         v-if="overlayStore.isActive"
-        :class="['fixed inset-0 bg-black/80', `z-[${overlayStore.z_index}]`]"
+        class="fixed inset-0 bg-black/80"
+        :style="{
+          zIndex: `${overlayStore.z_index}`
+        }"
         @click="() => overlayStore.close()"
       ></div>
     </transition>
