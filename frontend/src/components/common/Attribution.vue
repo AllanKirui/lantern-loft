39bb81aed58291981ad9a93e385763ddd531c270
Allan Kirui <allan.kirui@outlook.com>
2026-05-29 10:06:12 +0300

    feat(footer): add Attribution component

---
+++ frontend/src/components/common/Attribution.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{ linkColor: string }>()
+</script>
+
+<template>
+  <p>
+    Designed and built by
+    <a
+      class="link-hover"
+      :class="linkColor"
+      href="https://allankirui.github.io"
+      target="_blank"
+      rel="noreferrer"
+      data-replace="Allan Kirui"
+      ><span>Allan Kirui</span></a
+    >.
+  </p>
+</template>
