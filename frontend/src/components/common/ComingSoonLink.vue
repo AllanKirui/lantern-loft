2ed56d6bc50abbb1f33d0c38833afcd45f6e131a
Allan Kirui <allan.kirui@outlook.com>
2026-06-01 15:30:25 +0300

    feat(notifications): add ComingSoonLink component

---
+++ frontend/src/components/common/ComingSoonLink.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useNotificationStore } from "@/stores/notification"
+
+withDefaults(defineProps<{ type?: "link" | "button" }>(), {
+  type: "link"
+})
+
+const notificationStore = useNotificationStore()
+
+function handleClick(event: MouseEvent) {
+  event.preventDefault()
+
+  notificationStore.notify("Feature coming soon", "info", 3000)
+}
+</script>
+
+<template>
+  <template v-if="type === 'link'">
+    <a href="#" @click="handleClick">
+      <slot />
+    </a>
+  </template>
+
+  <template v-else>
+    <button @click="handleClick">
+      <slot />
+    </button>
+  </template>
+</template>
