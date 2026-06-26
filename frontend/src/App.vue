0e19a3c6e91edf04a1427251eb788c9921b1338f
Allan Kirui <allan.kirui@outlook.com>
2026-06-26 11:30:46 +0300

    feat(static-demo): integrate DemoBanner component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Notifications from "./components/common/Notifications.vue"
+import DemoBanner from "./components/app/DemoBanner.vue"
 </script>
 
 <template>
+  <DemoBanner />
   <RouterView />
   <Notifications />
 </template>
