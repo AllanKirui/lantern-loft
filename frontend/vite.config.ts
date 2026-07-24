4a1641a3233861790e96d32a4845187fd8d36f01
Allan Kirui <allan.kirui@outlook.com>
2026-07-24 15:34:11 +0300

    chore(vite): update @ alias that points to the src folder

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite"
+import { fileURLToPath } from "url"
 import vue from "@vitejs/plugin-vue"
-import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      "@": fileURLToPath(new URL("./src", import.meta.url))
     }
   }
 })
