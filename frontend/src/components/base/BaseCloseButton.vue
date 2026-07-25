ede0ee58c9fa7c6b4d013cb757171c05d36ec749
Allan Kirui <allan.kirui@outlook.com>
2026-07-25 16:32:20 +0300

    refactor(BaseCloseButton): replace integrated BaseIcon component with a slot

--- frontend/src/components/base/BaseCloseButton.vue
+++ frontend/src/components/base/BaseCloseButton.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
-withDefaults(defineProps<{ size?: string }>(), {
-  size: "18px"
-})
 const emit = defineEmits(["click"])
 </script>
 
 <template>
   <button class="btn-rounded" @click="$emit('click')">
-    <BaseIcon
-      name="close"
-      class="duration-200"
-      :stroke-width="2.5"
-      :size="size"
-    />
+    <slot />
     <span class="sr-only">Close</span>
   </button>
 </template>
